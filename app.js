4b180e791381882b2ab7439554c10e86922253df
Aymen <collaymen.alnamer@gmail.com>
2022-10-02 17:43:46 -0400

    commit

--- app.js
+++ app.js
@@ -1,61 +1,46 @@
 const form = document.querySelector("#main-page");
 const urlInput = document.getElementById("url-input");
-const topInput = document.querySelector("#top-text-input");
-const botInput = document.querySelector("#bottom-text-input");
+const topInput = document.getElementById("top-text-input");
+const botInput = document.getElementById("bottom-text-input");
 const meme = document.querySelector("#created-meme");
 const button = document.querySelector("#add-button");
+
+let counter = 0;
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
   if (urlInput.value.length === 0) {
     alert("URL Required");
-  } else if (
-    urlInput.value.length !== 0 &&
-    topInput.value.length === 0 &&
-    botInput.value.length === 0
-  ) {
+  } else {
     console.log("valid");
+    const container = document.createElement("div");
+    const hoverContainer = document.createElement("div");
+    const topContainer = document.createElement("div");
+    const bottomContainer = document.createElement("div");
     const newMeme = document.createElement("img");
     newMeme.src = urlInput.value;
-    meme.appendChild(newMeme);
+    if (topInput.value.length !== 0) {
+      topContainer.textContent = topInput.value;
+    }
+    if (botInput.value.length !== 0) {
+      bottomContainer.textContent = botInput.value;
+    }
+    container.id = counter + "";
+    hoverContainer.addEventListener("click", function (r) {
+      const selectedMeme = document.getElementById(container.id);
+      selectedMeme.remove();
+    });
+    counter++;
+    container.classList.add("new-meme");
+    hoverContainer.classList.add("new-meme-hover");
+    container.appendChild(hoverContainer);
+    container.appendChild(topContainer);
+    container.appendChild(newMeme);
+    container.appendChild(bottomContainer);
+    meme.appendChild(container);
+
     urlInput.value = "";
-  } else if (
-    urlInput.value.length !== 0 &&
-    topInput.value.length === 0 &&
-    botInput.value.length !== 0
-  ) {
-    console.log("valid");
-    const newMeme = document.createElement("img");
-    const newMemeBot = document.createElement("bottom-text");
-    newMeme.src = urlInput.value;
-    newMemeBot.src = botInput.value;
-    meme.appendChild(newMemeBot);
-    meme.appendChild(newMeme);
-    botInput.value = "";
-    urlInput.value = "";
-  } else if (
-    urlInput.value.length !== 0 &&
-    topInput.value.length !== 0 &&
-    botInput.value.length === 0
-  ) {
-    console.log("valid");
-    const newMeme = document.createElement("img");
-    const newMemeTop = document.createElement("top-text");
-    newMeme.src = urlInput.value;
-    meme.appendChild(newMeme);
-    newMemeTop.src = topInput.value;
-    meme.appendChild(newMemeTop);
     topInput.value = "";
-    urlInput.value = "";
-  } else if (
-    urlInput.value.length !== 0 &&
-    topInput.value.length !== 0 &&
-    botInput.value.length !== 0
-  ) {
-    console.log("valid");
-    const newMeme = document.createElement("img");
-    newMeme.src = urlInput.value;
-    meme.appendChild(newMeme);
-    urlInput.value = "";
+    botInput.value = "";
   }
 });
