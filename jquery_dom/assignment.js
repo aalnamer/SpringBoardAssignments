1dc244f889af128642748cc75e5abe97e9261be3
Aymen <collaymen.alnamer@gmail.com>
2022-11-09 15:49:35 -0500

    jquery assignment

---
+++ jquery_dom/assignment.js
@@ -0,0 +1,9 @@
+//Q1
+//When the DOM is ready, console.log the message “Let’s get ready to party with jQuery!”//
+
+
+$(function () {
+    console.log("Let's get ready to party with jQuery!");
+
+    
+
