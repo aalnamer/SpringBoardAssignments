c8b9578bce30bb78ea65566a2cb4d81ac01305e9
aymen computer <collaymen.alnamer@gmail.com>
2023-02-04 20:15:16 -0500

    oo python exercise

--- python_oo_practice/serial.py
+++ python_oo_practice/serial.py
@@ -1,7 +1,28 @@
 """Python serial number generator."""
 
 class SerialGenerator:
-    """Machine to create unique incrementing serial numbers.
+
+
+
+    def __init__(self, start = 0):
+        """ starts the generation of numbers from 0"""
+        self.start = self.next = start
+        
+
+    def generate(self):
+        """ adds 1 to the current value"""
+        self.next += 1
+        return self.next - 1
+        
+    
+    def reset(self):
+        """resets the value back to the starting value"""
+        self.next = self.start
+        
+
+        
+
+"""Machine to create unique incrementing serial numbers.
     
     >>> serial = SerialGenerator(start=100)
 
--- python_oo_practice/wordfinder.py
+++ python_oo_practice/wordfinder.py
@@ -1,5 +1,33 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+
+    def __init__(self,path):
+
+        words_path = open(path)
+        
+        path
+
+        self.words = self.convert(words_path)
+
+        print(f"{len(self.words)} words read")
+
+    def __repr__(self):
+        return f"WordFinder words found from file."
+    
+    def convert(self, words_path):
+        return [words.strip() for words in words_path]
+    
+    def random(self):
+        return random.choice(self.words)
+    
+
+
+
+class SpecialWordFinder(WordFinder):
+
+    def convert(self, words_path):
+        return [words.strip() for words in words_path
+                if words.strip() and not words.startswith("#")]
+    
